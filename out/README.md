eba46b23a0799934025ede477456aee475fafc4b
Will Humphreys <whumphreys@gmail.com>
2018-11-14 21:08:30 +0100

    Find the mode

--- out/README.md
+++ out/README.md
@@ -1,3 +1,15 @@
+##Setup
+
+You need to install Rcpp first with
+
+`install.packages("Rcpp",type="source")`
+
+and then pylr
+
+`install.packages('plyr')`
+
+
+
 ## To execute the node part
 
 `node index.js --in data/AUDUSD.csv --out out/AUDUSD-OUT.csv`
--- stats.r
+++ stats.r
@@ -1,7 +1,18 @@
+library(plyr)
+
 input <- 'out'
 file.name <- 'AUDUSD-OUT.csv'
 data.path <- file.path(input, file.name)
 data <- read.table(data.path, header=T,sep=",")
 
-print(sprintf("bestLow %s", max(data$bestLow)))
-print(sprintf("bestHigh %s", max(data$bestHigh)))
+
+
+highRow <- data[which.max(data$bestHigh),]
+
+lowRow <- data[which.max(data$bestLow),]
+
+print(sprintf("bestLow %s %s", lowRow$date, lowRow$bestLow))
+print(sprintf("bestHigh %s %s", highRow$date, highRow$bestHigh))
+
+count(data, 'bestHigh')
+count(data, 'bestLow')
